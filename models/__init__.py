6ad68a84320bb1f1a441f6a67916c8a9ef133c7d
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-15 20:40:47 +0800

    added visualization codes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .detr import build
+from .detr_vis import build_vis
 
 
 def build_model(args):
     return build(args)
+
+def build_vis_model(args):
+    return build_vis(args)
