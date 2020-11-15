8888f0514669c5f117095122b5afa80b3a6a8ae1
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-15 22:45:28 +0800

    added visualization codes

--- vis.py
+++ vis.py
@@ -204,11 +204,10 @@ for img_id in id_list:
         outputs = model(img, output_layer=output_layer)
 
         # keep only predictions with 0.7+ confidence
-        probas = outputs['pred_logits'].softmax(-1)
-        keep = probas.max(-1).values > 0.5
-        print(keep.shape)
-        # keep = [20, 92, 98]
-        # print(keep)
+        probas = outputs['pred_logits'].softmax(-1)[0,:,:,:]
+        # keep = probas.max(-1).values > 0.5
+        keep = [20, 92, 98]
+        print(keep)
 
 
         # convert boxes from [0; 1] to image scales
