285bbb7bfc4599b227acf833804b1afc20735eb8
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-19 17:41:58 +0800

    setup loggers

--- scripts/detr_r50_baseline_epoch150.sh
+++ scripts/detr_r50_baseline_epoch150.sh
@@ -2,7 +2,7 @@ script_name1=`basename $0`
 script_name=${script_name1:0:${#script_name1}-3}
 
 pip install termcolor --user
-MKL_SERVICE_FORCE_INTEL=1 python -m torch.distributed.launch \
+MKL_THREADING_LAYER=GNU python -m torch.distributed.launch \
     --nproc_per_node=8 \
     main.py \
     --coco_path ../data/coco \
