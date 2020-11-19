764da1b7d03f040f3dfb858182132c566bbd6dff
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-19 11:28:21 +0800

    setup loggers

--- test_logger.py
+++ test_logger.py
@@ -4,6 +4,6 @@ from test_logger_2 import test_proc
 
 
 
-logger = setup_logger(output='output/test_logger', distributed_rank=dist.get_rank(), name="DETR", phase="train")
+logger = setup_logger(output='output/test_logger', distributed_rank=0, name="DETR", phase="train")
 logger.info("hello")
 test_proc()
