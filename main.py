640b87ada4544cc8b0f02e0c5ddbdfc3ccd206ad
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-23 16:59:12 +0800

    load changed to strict to failse

--- main.py
+++ main.py
@@ -178,9 +178,9 @@ def main(args):
     if args.resume:
         if args.resume.startswith('https'):
             checkpoint = torch.hub.load_state_dict_from_url(
-                args.resume, map_location='cpu', check_hash=True)
+                args.resume, map_location='cpu', check_hash=True, strict=False)
         else:
-            checkpoint = torch.load(args.resume, map_location='cpu')
+            checkpoint = torch.load(args.resume, map_location='cpu', strict=False)
         model_without_ddp.load_state_dict(checkpoint['model'])
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
