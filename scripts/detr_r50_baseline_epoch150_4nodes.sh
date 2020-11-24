26f0012959cf8e95aab978658110471d9715e8ab
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-24 15:40:39 +0800

    fixing multi-node script

--- scripts/detr_r50_baseline_epoch150_4nodes.sh
+++ scripts/detr_r50_baseline_epoch150_4nodes.sh
@@ -12,6 +12,7 @@ script_name1=`basename $0`
 script_name=${script_name1:0:${#script_name1}-3}
 
 pip install termcolor --user
+pip list
 MKL_THREADING_LAYER=GNU python -m torch.distributed.launch \
     --nproc_per_node=8 \
     --nnodes=4 \
