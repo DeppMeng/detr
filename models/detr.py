458850eab77b3012c55c188690d169dc17f1f9e0
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-25 00:21:10 +0800

    adding posembedv2

--- models/detr.py
+++ models/detr.py
@@ -91,7 +91,7 @@ class DETR(nn.Module):
             self.query_embed = nn.Embedding(self.num_queries, self.hidden_dim)
             upsamp = nn.Upsample(size=(10, 10), mode=self.sine_query_embed_mode)
             example_tensor = NestedTensor(tensors=torch.zeros((4, 2048, 41, 41)).cuda(), mask=torch.ones((4, 41, 41), dtype=torch.bool).cuda())
-            pos_temp = self.backbone[1](example_tensor).to(example_tensor.tensors.dtype)[-1]
+            pos_temp = self.backbone[1](example_tensor).to(example_tensor.tensors.dtype)
             print(pos[-1].shape)
             print(pos_temp.shape)
             pos_embed_example = upsamp(pos_temp)
