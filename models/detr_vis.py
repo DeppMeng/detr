212ebaf6458908ba7cea4ae271ff74bcdbe10f54
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-25 20:05:07 +0800

    added scripts

--- models/detr_vis.py
+++ models/detr_vis.py
@@ -75,7 +75,7 @@ class DETR(nn.Module):
     
             self.query_embed = nn.Embedding(self.num_queries, self.hidden_dim)
             upsamp = nn.Upsample(size=(10, 10), mode=self.sine_query_embed_mode)
-            example_tensor = NestedTensor(tensors=torch.zeros((4, 2048, 10, 10)).cuda(), mask=torch.ones((4, 10, 10), dtype=torch.bool).cuda())
+            example_tensor = NestedTensor(tensors=torch.zeros((4, 2048, 20, 20)).cuda(), mask=torch.ones((4, 20, 20), dtype=torch.bool).cuda())
             pos_temp = self.backbone[1](example_tensor).to(example_tensor.tensors.dtype)
 
 
