48966ea442f10487a91784a695db5afefafefe7c
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-11-25 19:56:14 +0800

    added scripts

--- models/detr.py
+++ models/detr.py
@@ -66,9 +66,17 @@ class DETR(nn.Module):
             samples = nested_tensor_from_tensor_list(samples)
         features, pos = self.backbone(samples)
         if self.sine_query_embed == True and self.query_embed == None:
+            # self.query_embed = nn.Embedding(self.num_queries, self.hidden_dim)
+            # upsamp = nn.Upsample(size=(10, 10), mode='bilinear')
+            # pos_embed_example = upsamp(pos[-1])
+            # self.query_embed.weight = torch.nn.Parameter(pos_embed_example.flatten(2)[0].squeeze(0).permute(1, 0))
+            # self.query_embed.weight.requires_grad = False
+            
             self.query_embed = nn.Embedding(self.num_queries, self.hidden_dim)
-            upsamp = nn.Upsample(size=(10, 10), mode='bilinear')
-            pos_embed_example = upsamp(pos[-1])
+            upsamp = nn.Upsample(size=(10, 10), mode=self.sine_query_embed_mode)
+            example_tensor = NestedTensor(tensors=torch.zeros((4, 2048, 41, 41)).cuda(), mask=torch.ones((4, 41, 41), dtype=torch.bool).cuda())
+            pos_temp = self.backbone[1](example_tensor).to(example_tensor.tensors.dtype)
+            pos_embed_example = upsamp(pos_temp)
             self.query_embed.weight = torch.nn.Parameter(pos_embed_example.flatten(2)[0].squeeze(0).permute(1, 0))
             self.query_embed.weight.requires_grad = False
 
