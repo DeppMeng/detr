92c634e4a66c8ab4fdbd4195ea0c1c0d4f2b056a
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-12-03 16:32:20 +0800

    added scripts

--- models/detr.py
+++ models/detr.py
@@ -302,7 +302,7 @@ class SetCriterion(nn.Module):
 
         # Retrieve the matching between the outputs of the last layer and the targets
         indices = self.matcher(outputs_without_aux, targets)
-        print(indices.shape)
+        # print(indices)
         print(indices)
 
         # Compute the average number of target boxes accross all nodes, for normalization purposes
