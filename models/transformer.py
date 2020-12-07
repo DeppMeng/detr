2270105ea38719e6c1438cadb8a35dc8269ac40a
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-12-08 01:54:15 +0800

    testing disentangledv1

--- models/transformer.py
+++ models/transformer.py
@@ -462,8 +462,8 @@ class DisentangledV1Transformer(nn.Module):
         mask = mask.flatten(1)
 
         tgt_cls = torch.zeros_like(query_embed_cls)
-        print(tgt_cls.shape)
-        tgt_reg = torch.zeros_like(query_embed_cls // 4)
+        tgt_shape = tgt_cls.shape
+        tgt_reg = torch.zeros_like((tgt_shape[0], tgt_shape[1], tgt_shape[2] // 4))
         memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
         hs_cls = self.decoder_cls(tgt_cls, memory, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed_cls).transpose(1, 2)
