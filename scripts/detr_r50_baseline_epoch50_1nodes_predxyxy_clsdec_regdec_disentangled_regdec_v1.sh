71bb1839302f8d50894de5bfaa5da45735a4455e
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-12-08 11:14:09 +0800

    testing disentangledv1

---
+++ scripts/detr_r50_baseline_epoch50_1nodes_predxyxy_clsdec_regdec_disentangled_regdec_v1.sh
@@ -0,0 +1,27 @@
+NCCL_SOCKET_IFNAME=ib0
+
+# MASTER_IP=${AZ_BATCH_MASTER_NODE%:*} && echo MASTER_IP: ${MASTER_IP}
+# MASTER_PORT=${AZ_BATCH_MASTER_NODE##*:} && echo MASTER_PORT: ${MASTER_PORT}
+MASTER_IP=${MASTER_IP}
+MASTER_PORT=1234
+NODE_RANK=${OMPI_COMM_WORLD_RANK} && echo NODE_RANK: ${NODE_RANK}
+PER_NODE_GPU=8 && echo PER_NODE_GPU: ${PER_NODE_GPU}
+NUM_NODE=${OMPI_COMM_WORLD_SIZE} && echo NUM_NODE: ${NUM_NODE}
+
+script_name1=`basename $0`
+script_name=${script_name1:0:${#script_name1}-3}
+
+pip install termcolor --user
+pip list
+MKL_THREADING_LAYER=GNU python -m torch.distributed.launch \
+    --nproc_per_node=8 \
+    --use_env \
+    main.py \
+    --resume auto \
+    --epochs 50 \
+    --lr_drop 40 \
+    --pred_xyxy \
+    --clsdec_regdec \
+    --disentangled_regdec_v1 \
+    --coco_path ../data/coco \
+    --output_dir output/$script_name
